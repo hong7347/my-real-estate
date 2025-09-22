18fb4548355d421748aeaef45986094d380d87e5
hong <estates7347@gmail.com>
2025-09-22 15:11:22 +0900

    feat: /admin/properties page

---
+++ src/app/admin/properties/page.tsx
@@ -0,0 +1,13 @@
+
+export default function AdminPropertiesPage() {
+  return <h1>관리자 - 매물 관리</h1>;
+}
+
+/**
+ * (참고) 나중에 서버 액션을 넣고 싶다면 이렇게 “함수 안”에서만 사용하세요.
+ * 페이지 파일 맨 위에 'use server'를 넣으면 지금 같은 오류가 납니다.
+ */
+// async function createProperty(formData: FormData) {
+//   'use server';
+//   // ...서버 액션 로직...
+// }
